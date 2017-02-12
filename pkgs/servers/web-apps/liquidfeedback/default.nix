c9d40b7ca326688614212193f6ccb7a3b7e44693
Franz Pletz <fpletz@fnordicwalking.de>
2017-02-13 00:00:29 +0100

    foo

--- pkgs/servers/web-apps/liquidfeedback/default.nix
+++ pkgs/servers/web-apps/liquidfeedback/default.nix
@@ -1,50 +1,55 @@
 { stdenv, fetchurl, lua5, postgresql, moonbridge, webmcp}:
 
-stdenv.mkDerivation rec {
-  version  ="3.2.2";
+let
+  version = "3.2.2";
   frontendversion = "3.2.1";
 
-  name = "liquidfeedback-${version}";
+  ldap4lqfb = fetchurl {
+    url = "http://www.public-software-group.org/pub/projects/liquid_feedback/contrib/ldap4lqfb/ldap4lqfb.tar.gz";
+    sha256  ="0flm2hc5jlni62y453bqrqscdc3a8qzkd4rbia39ha3ic9jd8hq8";
+  };
 
-  core = fetchurl {
+  core = stdenv.mkDerivation {
+    name = "liquidfeedback-core-${version}";
+
+    src = fetchurl {
       url = "http://www.public-software-group.org/pub/projects/liquid_feedback/backend/v${version}/liquid_feedback_core-v${version}.tar.gz";
       sha256 = "1jy1c9y44j5abydhx1c8h03hswign9iy966xrgisxza6ji0ylz41";
+    };
+
+    buildInputs = [
+      postgresql
+    ];
+
+    installPhase = ''
+      mkdir -p $out/sql
+      mkdir -p $out/bin
+      cp core.sql $out/sql/
+      cp lf_update $out/bin
+      cp lf_update_issue_order $out/bin
+      cp lf_update_suggestion_order $out/bin
+    '';
   };
 
-  frontend = fetchurl {
-      url = "http://www.public-software-group.org/pub/projects/liquid_feedback/frontend/v${frontendversion}/liquid_feedback_frontend-v${frontendversion}.tar.gz";
-      sha256 = "07mvlvis0gjv55hr5a5w2ni5cys507kv7pa2bkd0mc3b49gz6mgc";
-  };
+in
 
-  ldap4lqfb = fetchurl {
-      url = "http://www.public-software-group.org/pub/projects/liquid_feedback/contrib/ldap4lqfb/ldap4lqfb.tar.gz";
-      sha256  ="0flm2hc5jlni62y453bqrqscdc3a8qzkd4rbia39ha3ic9jd8hq8";
-  };
+stdenv.mkDerivation rec {
+  name = "liquidfeedback-${version}";
 
-  srcs = [ core frontend ldap4lqfb];
+  src = fetchurl {
+    url = "http://www.public-software-group.org/pub/projects/liquid_feedback/frontend/v${frontendversion}/liquid_feedback_frontend-v${frontendversion}.tar.gz";
+    sha256 = "07mvlvis0gjv55hr5a5w2ni5cys507kv7pa2bkd0mc3b49gz6mgc";
+  };
 
   buildInputs = [
-   lua5 postgresql
+    lua5
   ];
 
-  sourceRoot = ".";
-
-  preBuild = ''
-     cd ${sourceRoot}/liquid_feedback_core-v${version}
-     make
-  '';
-
   installPhase = ''
-	mkdir -p $out/core/sql
-	mkdir -p $out/core/bin
-	cp $sourceRoot/liquid_feedback_core-v${version}/core.sql $out/core/sql/
-    cp $sourceRoot/liquid_feedback_core-v${version}/lf_update $out/core/bin
-    cp $sourceRoot/liquid_feedback_core-v${version}/lf_update_issue_order $out/core/bin
-    cp $sourceRoot/liquid_feedback_core-v${version}/lf_update_suggestion_order $out/core/bin/
+    cp -r ${ldap4lqfb}/* ..
 
     mkdir -p $out/frontend
-    cp -r $out/liquid_feedback_frontend-v${frontendversion} $out/frontend/    
-
+    cp -r $out/liquid_feedback_frontend-v${frontendversion} $out/frontend/
   '';
 
   meta = with stdenv.lib; {
